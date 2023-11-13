3f0e0b066442ab2ed9c2767f947bbf7a1eabaeed
laaraff <lara_raffetti@gmail.com>
2023-11-13 10:04:04 -0300

    mas_avances_y_problemas

--- Borrador_TPfinal.R
+++ Borrador_TPfinal.R
@@ -1,40 +1,47 @@
 #Comienzo borrador Trabajo final
 
-require(ncdf4)
-nc<-nc_open ("/home/clinux01/Escritorio/Lara/Trabajo_Final/icec.sfc.mon.mean.nc")
-nc
 
+# ITEM A - Leer el archivo y extraer info ---------------------------------
+
+require(ncdf4)
 library(udunits2)
 library(metR)
 library(lubridate)
+library(ggplot2)
 
-archivo <- "/home/clinux01/Escritorio/Lara/Trabajo_Final/icec.sfc.mon.mean.nc"
+#nc<-nc_open ("/home/clinux01/Escritorio/Lara/Trabajo_Final/icec.sfc.mon.mean.nc") #LINUX
+nc <- nc_open("C:/Users/Usuario/Documents/Lara/Trabajo_Final_Hielo_Marino/icec.sfc.mon.mean.nc") #WINDOWS
+nc
+
+archivo <- "C:/Users/Usuario/Documents/Lara/Trabajo_Final_Hielo_Marino/icec.sfc.mon.mean.nc"
 GlanceNetCDF(archivo)
 datos <- ReadNetCDF(archivo, vars = "icec")
 hist(datos$icec)
 dim(datos) #16405632  4 Es un dataframe
 
-#Recorto la region
+
+
+# ITEM B - Selecciono Antártida período 1990-2019 ---------------
+
+antartida <- ReadNetCDF(archivo, vars = "icec",
+                        subset = list(lat = c(-60,-88), #va hasta -88
+                                      lon = c(0,180))) #REVISAR ESTE RECORTE!!!!
+
+datos_antartida_periodo<- antartida[which(year(antartida$time) %in% 1990:2019),]
+
+# ITEM C - Selecciono las bases de interés. Serie temporal ----------------
 
 datos_esperanza_totales<- ReadNetCDF(archivo, vars = "icec",
                           subset = list(lat = c(-63.396958),
                                         lon = c(360-56.998053)))
-datos_belgrano_totales <- ReadNetCDF(archivo, vars = "icec",
-                             subset = list(lat = c(-77.874443),
-                                           lon = c(360-34.626943)))
+datos_davis_totales <- ReadNetCDF(archivo, vars = "icec",
+                                  subset = list(lat = c(-68.469637),
+                                                lon = c(360-78.790891)))
+
 
 #Recorto los anios
 datos_esperanza_periodo<- datos_esperanza_totales[which(year(datos_esperanza_totales$time) %in% 1990:2019),]
 head(datos_esperanza_periodo)
-
-datos_belgrano_periodo <- datos_belgrano_totales[which(year(datos_belgrano_totales$time) %in% 1990:2019),]
-head(datos_belgrano_periodo)
-
-
-#Veo que onda la base Davis
-datos_davis_totales <- ReadNetCDF(archivo, vars = "icec",
-                                 subset = list(lat = c(-68.469637),
-                                               lon = c(360-78.790891)))
 
 datos_davis_periodo <- datos_davis_totales[which(year(datos_davis_totales$time) %in% 1990:2019),]
 
@@ -42,28 +49,42 @@ datos_davis_periodo <- datos_davis_totales[which(year(datos_davis_totales$time) 
 #c)
 #Para Esperanza
 
-#Serie temporal. DESCARTADO, SE VE HORRIBLE
-#grafico <- ggplot(data= datos_esperanza_periodo, mapping= aes(x=time, y=icec)) +
-#  geom_line(color="black") +
-#  labs(title= "Evolución del hielo marino Base Esperanza", subtitle= "Periodo 1990-2019", x= "Mes", y= "Hielo marino (%)")
 
 #Pruebo con barras. Esperanza
 grafico <- ggplot(data= datos_esperanza_periodo, mapping= aes(x=time, y=icec)) +
   geom_bar(stat="identity", color="#acd8fa") +
-  labs(title= "Evolución del hielo marino Base Esperanza", subtitle= "Periodo 1990-2019", x= "Mes", y= "Hielo marino (%)")
+  labs(title= "Variación de la concentración del hielo marino Base Esperanza (Argentina)", subtitle= "Periodo 1990-2019", x= "Mes", y= "Hielo marino (%)")
 
 #Para Davis
 grafico1 <- ggplot(data= datos_davis_periodo, mapping= aes(x=time, y=icec)) +
   geom_bar(stat="identity", color="#33a6ff") +
-  labs(title= "Evolución del hielo marino Base Davis", subtitle= "Periodo 1990-2019", x= "Mes", y= "Hielo marino (%)")
+  labs(title= "Variación de la concentración del hielo marino Base Davis (Australia)", subtitle= "Periodo 1990-2019", x= "Mes", y= "Hielo marino (%)")
 
 
+#Almaceno la info en una tabla .txt NO PONGO LOS TIEMPOS?????
+
+tabla_esperanza <- datos_esperanza_periodo
+tabla_esperanza$time <- NULL
+colnames(tabla_esperanza) <- c("Latitud", "Longitud", "Concentracion")
+
+write.table(tabla_esperanza, file= "datos_esperanza.txt", sep="  ", row.names = F)
+
+#Hago lo mismo para la base Davis
+tabla_davis <- datos_davis_periodo
+tabla_davis$time <- NULL
+colnames(tabla_davis) <- c("Latitud", "Longitud", "Concentracion")
+
+write.table(tabla_davis, file= "datos_davis.txt", sep="  ", row.names = F)
 
 
+# ITEM D - Climatologia mensual para todo la region ----------------------
+#primero reemplazo la columna time solo por el mes
+datos_antartida_periodo$time <- month(datos_antartida_periodo$time)
+media_por_meses <- aggregate(datos_antartida_periodo$icec, list(datos_antartida_periodo$time), mean)
 
-#Almaceno la info en una tabla (del item c) FALTA GUARDARLA EN FORMATO TXT
+colnames(media_por_meses) <- c("Mes", "Media")
 
-
+#si lo quiero ordenar tengo que tener cuidado que no ordena las medias tmb
 
 
 
