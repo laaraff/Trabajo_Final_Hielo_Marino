f59d4827f32357c20c51099e89441a7efdc403c9
laaraff <lara_raffetti@gmail.com>
2023-11-24 15:13:41 -0300

    casi_termiando!

--- Borrador_TPfinal.R
+++ Borrador_TPfinal.R
@@ -10,12 +10,12 @@ library(lubridate)
 library(ggplot2)
 
 #nc<-nc_open ("/home/clinux01/Escritorio/Lara/Trabajo_Final_Hielo_Marino/icec.sfc.mon.mean.nc") #LINUX
-nc<-nc_open ("/home/clinux01/Escritorio/Lara/Trabajo_Final/icec.sfc.mon.mean.nc") #LINUX COMPU JUEVES
-#nc <- nc_open("C:/Users/Usuario/Documents/Lara/Trabajo_Final_Hielo_Marino/icec.sfc.mon.mean.nc") #WINDOWS
+#nc<-nc_open ("/home/clinux01/Escritorio/Lara/Trabajo_Final/icec.sfc.mon.mean.nc") #LINUX COMPU JUEVES
+nc <- nc_open("C:/Users/Usuario/Documents/Lara/Trabajo_Final_Hielo_Marino/icec.sfc.mon.mean.nc") #WINDOWS
 nc
 
 # WINDOWS archivo <- "C:/Users/Usuario/Documents/Lara/Trabajo_Final_Hielo_Marino/icec.sfc.mon.mean.nc"
-archivo <- "/home/clinux01/Escritorio/Lara/Trabajo_Final/icec.sfc.mon.mean.nc" #Ojo cambio nombre carpeta
+archivo <- "C:/Users/Usuario/Documents/Lara/Trabajo_Final_Hielo_Marino/icec.sfc.mon.mean.nc" #Ojo cambio nombre carpeta
 GlanceNetCDF(archivo)
 datos <- ReadNetCDF(archivo, vars = "icec")
 dim(datos) #16405632  4 Es un dataframe
@@ -163,8 +163,6 @@ g <- g+coord_map(projection = proy,orientation=orientacion, ylim = c(-40, -90))
 
 
 
-
-
 # ITEM E - CORRELACION CON INDICE SOI -------------------------------------
 
 datos_soi <- read.table(file= "soi.txt", skip= 43, nrows = 30) #Abro el archivo y recorto los datos del periodo que necesito: 1990-2019.
@@ -215,6 +213,8 @@ colnames(SON_soi) <- c("Anio","PromEstacion")
 DEF_soi <- promedio_soi_diciembre("DEF")
 colnames(DEF_soi) <- c("Anio","PromEstacion")
 
+DEF_soi <- DEF_soi[-1,] #borro la primera fila porque tiene NAs (no tengo datos del verano de 1990)
+
 
 #Hago promedio estacional para la Base Esperanza
 datos_esperanza_periodo$mes <- month(datos_esperanza_periodo$time)
@@ -222,7 +222,7 @@ datos_esperanza_periodo$anio <- year(datos_esperanza_periodo$time)
 
 #MAM
 mam <- sort(c(which(datos_esperanza_periodo$mes==3), which(datos_esperanza_periodo$mes==4),which(datos_esperanza_periodo$mes==5))) #posiciones de los meses 3,4 y 5
-esperanza_mam <- aggregate(datos_esperanza_periodo$icec[mam], list(datos_esperanza_periodo$anio[mam]), mean)
+esperanza_mam <- aggregate(datos_esperanza_periodo$icec[mam], list(datos_esperanza_periodo$anio[mam]), mean) #hago los promedios para esos meses
 
 #JJA (misma idea que antes)
 jja <- sort(c(which(datos_esperanza_periodo$mes==6), which(datos_esperanza_periodo$mes==7),which(datos_esperanza_periodo$mes==8))) 
@@ -236,17 +236,19 @@ esperanza_son <- aggregate(datos_esperanza_periodo$icec[son], list(datos_esperan
 #Antes recorto los meses de 1990 que no voy a usar, ya que el primer verano que tengo es 1991
 datos_esperanza_periodo1<-datos_esperanza_periodo[-c(1:11),]
 
+
 #Ahora voy a hacer una trampita y asignar cada diciembre al año siguiente para poder hacer mas facil los promedios del verano
-datos_esperanza_periodo2 <- datos_esperanza_periodo1 #hago otro dataframe para no 
-diciembres <- which(datos_esperanza_periodo2$mes==12)
-datos_esperanza_periodo2$anio[diciembres]<-datos_esperanza_periodo2$anio[diciembres]+1
+datos_esperanza_periodo2 <- datos_esperanza_periodo1 #hago otro dataframe para no reescribir
+diciembres <- which(datos_esperanza_periodo2$mes==12) #las posiciones de los diciembres
+datos_esperanza_periodo2$anio[diciembres]<-datos_esperanza_periodo2$anio[diciembres]+1 #las asigno al anio siguiente
+datos_esperanza_periodo2 <- datos_esperanza_periodo2[-349,]#y borro dicimebre de 2019 que no lo voy a usar porque corresponde al verano del 2020
 
 def <-sort(c(diciembres, which(datos_esperanza_periodo2$mes==1),which(datos_esperanza_periodo2$mes==2))) 
 esperanza_def <- aggregate(datos_esperanza_periodo2$icec[def], list(datos_esperanza_periodo2$anio[def]), mean)
 
 
 #Hago las correlaciones
-cor_verano_esp <-       #VOY A TENER QUE AGREGAR UN NA.RM=T
+cor_verano_esp <- cor(DEF_soi[,2], esperanza_def[,2]) #Agrego el na.rm porque en los veranos no tengo datos de 1990, entonces hay un NA
 cor_otonio_esp <- cor(MAM_soi[,2], esperanza_mam[,2])
 cor_invierno_esp <- cor(JJA_soi[,2], esperanza_jja[,2])
 cor_primavera_esp <-cor(SON_soi[,2], esperanza_son[,2])
@@ -254,51 +256,102 @@ cor_primavera_esp <-cor(SON_soi[,2], esperanza_son[,2])
 #Armo un dataframe con esa info y lo guardo en formato txt.
 season <- c("DEF", "MAM", "JJA","SON")
 cor_esp <- c(cor_verano_esp, cor_otonio_esp, cor_invierno_esp, cor_primavera_esp)
-correlacion_esperanza <- data.frame("Season"= season, "Coeficiente de correlacion SOI/Concentracion hielo marino Esperanza" = cor_esp)
+correlacion_esperanza <- data.frame("Season"= season, "Coeficiente de correlacion SOI/Concentracion hielo marino Base Esperanza" = cor_esp)
 write.table(correlacion_esperanza, file= "correlacion_soi_hielo_esperanza.txt", sep="  ", row.names = F)
 
 
 
-#Idem a lo anterior pero con la base Davis
-#MAM
-mam1 <- sort(c(which(datos_davis_periodo$mes==3), which(datos_davis_periodo$mes==4),which(datos_davis_periodo$mes==5))) #posiciones de los meses 3,4 y 5 OJ
-davis_mam <- aggregate(datos_davis_periodo$icec[mam1], list(datos_davis_periodo$anio[mam1]), mean)
+#Idem a todo lo anterior pero con la base Davis
+datos_davis_periodo$mes <- month(datos_davis_periodo$time)
+datos_davis_periodo$anio <- year(datos_davis_periodo$time)
+
+#MAM. Las posiciones de los meses van a ser iguales a las de Esperanza ya que tengo la misma cantidad de datos
+davis_mam <- aggregate(datos_davis_periodo$icec[mam], list(datos_davis_periodo$anio[mam]), mean)
 
 #JJA
-
-
+davis_jja <- aggregate(datos_davis_periodo$icec[jja], list(datos_davis_periodo$anio[jja]), mean)
 
 #SON
+davis_son <- aggregate(datos_davis_periodo$icec[son], list(datos_davis_periodo$anio[son]), mean)
 
+#DEF. Mismo truco que antes
+datos_davis_periodo1<-datos_davis_periodo[-c(1:11),]
+datos_davis_periodo1$anio[diciembres]<-datos_davis_periodo1$anio[diciembres]+1 #las asigno al anio siguiente. Posiciones de diciembre son iguales a Esperanza
 
-#DEF
+datos_davis_periodo1 <- datos_davis_periodo1[-349,]#y borro dicimebre de 2019 que no lo voy a usar porque corresponde al verano del 2020
+
+davis_def <- aggregate(datos_davis_periodo1$icec[def], list(datos_davis_periodo1$anio[def]), mean)
 
 #Hago las correlaciones, guardo la info en un dataframe y lo guardo en un archivo .txt
+cor_verano_dav <- cor(DEF_soi[,2], davis_def[,2]) #Agrego el na.rm porque en los veranos no tengo datos de 1990, entonces hay un NA
+cor_otonio_dav <- cor(MAM_soi[,2], davis_mam[,2])
+cor_invierno_dav <- cor(JJA_soi[,2], davis_jja[,2])
+cor_primavera_dav <-cor(SON_soi[,2], davis_son[,2])
+
+#Armo un dataframe con esa info y lo guardo en formato txt.
+season <- c("DEF", "MAM", "JJA","SON")
+cor_dav <- c(cor_verano_dav, cor_otonio_dav, cor_invierno_dav, cor_primavera_dav)
+correlacion_davis <- data.frame("Season"= season, "Coeficiente de correlacion SOI/Concentracion hielo marino Base Davis" = cor_dav)
+write.table(correlacion_davis, file= "correlacion_soi_hielo_davis.txt", sep="  ", row.names = F)
 
 
 
 
 #------------------------------------------------------
-#Para marcar las estaciones en un mapa (en proceso)
+#EXTRA
 #Hago un mapa para mostrar donde estan las bases con las que estoy trabajando. 
 coordenadas <- data.frame(
   latitud = c(-63.8079, -68.46),
   longitud = c(-56.99, 78.86)
 )
+etiquetas <- c("Base Esperanza (Argentina)", c("Base Davis (Australia)"))
+
 
 south_america_map <- map_data("world", continent = "South America")
 antarctica_map <- map_data("world", region = "Antarctica")
-color= c("blue", "green")
+color= c("#FE006C", "#FE006C")
+
+mapa_estaciones <- ggplot() +
+  geom_polygon(data = data.frame(long = c(-180, 180, 180, -180), lat = c(-84, -84, 0, 0)), aes(x = long, y = lat), fill = "#ADEEFF") + #fondo
+  geom_polygon(data = south_america_map, aes(x = long, y = lat, group = group), fill = "#E9F0F0", color = "black") +
+  geom_polygon(data = antarctica_map, aes(x = long, y = lat, group = group), fill = "#E9F0F0", color = "black") +
+  geom_point(data = coordenadas, aes(x = longitud, y = latitud, color = color), size = 3) +
+  geom_text(data = coordenadas, aes(x = longitud, y = latitud, label = etiquetas), vjust = -2, nudge_y = 0.5, size = 4.5, fontface = c("bold")) +
+  coord_cartesian(xlim = c(-100, 100), ylim = c(-84, -40)) +
+  theme_minimal() +
+  labs(title = "Ubicación de las bases antárticas trabajadas", size=10) +
+  scale_color_identity()
+
+mapa_estaciones
+
+
+#Y con proyeccion polar
+mapa <- map_data("world")
+proy<-"stereographic"
+orientacion<-c(-90,0,0)
 
 
 ggplot() +
-  geom_polygon(data = south_america_map, aes(x = long, y = lat, group = group), fill = "lightblue", color = "black") +
-  geom_polygon(data = antarctica_map, aes(x = long, y = lat, group = group), fill = "lightblue", color = "black") +
-  geom_point(data = coordenadas, aes(x = longitud, y = latitud, color = color), size = 3) +
-  coord_cartesian(xlim = c(-100, 100), ylim = c(-90, -40)) +  # Definir límites de visualización
-  theme_minimal() +
-  labs(title = "Ubicación de las bases antárticas trabajadas") +
-  scale_color_identity()  #ver esto!!! 
-#usar el geom_text para escribir las etiquetas de las bases
+  geom_polygon(data = mapa, aes(x = long, y = lat, group = group), fill = "#E9F0F0", color = "black") + #pinto los continentes
+  geom_point(data = coordenadas, aes(x = longitud, y = latitud, color = "red"), size = 3, shape= 15) +
+  geom_text(
+    data = coordenadas,
+    aes(x = longitud, y = latitud, label = etiquetas, fontface= "bold", family= "serif"),
+    position = position_nudge(x= 10, y = 5) 
+    
+  ) +
+  geom_path(
+    data = mapa,
+    aes(long, lat, group = group),
+    linewidth = 0.4
+  ) +
+  labs(title = "Ubicación de las bases trabajadas (proyección polar)") +
+  scale_y_continuous(limits = c(-90, 0)) +
+  coord_map(projection = proy, orientation = orientacion, ylim = c(-40, -90)) +
+  theme_bw() + 
+  theme(
+    panel.background = element_rect(fill = "#ADEEFF") 
+  ) 
+
 
 
